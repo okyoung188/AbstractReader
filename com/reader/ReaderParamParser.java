9220b4a76cf9fb10636280cd1f0614679e6ca9e9
harry <18817504792@126.com>
2017-07-04 03:44:40 -0700

    XmlFileProperty defined

--- com/reader/ReaderParamParser.java
+++ com/reader/ReaderParamParser.java
@@ -1,66 +1,46 @@
 package com.reader;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
 
+import java.io.InputStream;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.dom4j.io.SAXReader;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 public class ReaderParamParser {
+
+    String defaultXmlPath = "prop/reader.xml";
+
 	Document document;
-	
+
 	List<String> processors;
 
-	public ReaderParamParser() {
-		super();
+	/**
+	 * Get the reader xml default path
+	 * @return default reader xml path
+	 */
+	public String getDefaultXmlPath() {
+		return defaultXmlPath;
 	}
 
-	public void parseParam() throws Exception {
-		
-		
-		InputStream inStream = ClassLoader.getSystemResourceAsStream("prop/reader.xml");
-		BufferedReader buffReader = new BufferedReader(new InputStreamReader(inStream));
-		String line = null;
-		while((line = buffReader.readLine()) != null){
-			System.out.println(line);
-		}
-		buffReader.close();
-		inStream.close();
-		
-		inStream = ClassLoader.getSystemResourceAsStream("prop/reader.xml");
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		
-		System.out.println(factory instanceof javax.xml.parsers.DocumentBuilderFactory);
-		System.out.println(factory instanceof com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl);
-		
-		document = builder.parse(inStream);
-		System.out.println(document.getNamespaceURI());
-		System.out.println(document.getChildNodes());
-		System.out.println(document.getDocumentElement());
-		System.out.println(document.getDocumentElement().getChildNodes().getLength());
-		
-		inStream = ClassLoader.getSystemResourceAsStream("prop/reader.xml");
-		SAXReader reader = new SAXReader();
-		org.dom4j.Document doc = reader.read(inStream);
-		System.out.println(doc);
-		
-		
+	/**
+	 * Set the reader xml path
+	 * @param defaultXmlPath default reader xml path
+	 */
+	public void setDefaultXmlPath(String defaultXmlPath) {
+		this.defaultXmlPath = defaultXmlPath;
 	}
-	
-    public List<String> getProcessors(){
-    	return processors;
-    }
-    
-    public static void main(String[] args) throws Exception {
-		ReaderParamParser parser = new ReaderParamParser();
-		parser.parseParam();
+
+	public void setProcessors(List<String> processors) {
+		this.processors = processors;
+	}
+
+	public List<String> getProcessors() {
+		return processors;
 	}
 
 	public Document getDocument() {
@@ -70,9 +50,70 @@ public class ReaderParamParser {
 	public void setDocument(Document document) {
 		this.document = document;
 	}
-    
-    
 
+	public ReaderParamParser() {
+		super();
+	}
 	
-	
+	/**
+	 * Parse reader xml configuration file
+	 * @throws Exception
+	 */
+	public void parseParam() throws Exception {
+		InputStream inStream = ClassLoader.getSystemResourceAsStream(defaultXmlPath);
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		document = builder.parse(inStream);
+		Element element = document.getDocumentElement();
+        NodeList nodeList = element.getChildNodes();
+        for (int i=0;i<nodeList.getLength();i++){
+        	Node node = nodeList.item(i);
+        	short nodeType = node.getNodeType(); 
+        	if(nodeType == Node.ELEMENT_NODE){
+        		String nodeName = node.getNodeName();
+        		if(nodeName.equals(XmlFileProperty.CITY)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.CITY_SET)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.CONNECT_TIME_OUT)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.DATA_URL)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.DATA_URL_LIST)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.EXTRACTER)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.FORMATTER)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.MAP_URL)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.PROCESSOR)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.READER_PARAM)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.REFINER)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.REQUESTER)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.RETRY_TIME_OUT)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.REVERSE_GEOCODING_FLAG)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.SLEEP_TIME_OUT)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.STATE)){
+        			
+        		} else if(nodeName.equals(XmlFileProperty.TC_SEPARATE_SIGN)){
+        			
+        		} else {// other config
+        			
+        		}
+        	}
+        }
+	}
+
+	public static void main(String[] args) throws Exception {
+		ReaderParamParser parser = new ReaderParamParser();
+		parser.parseParam();
+	}
 }
---
+++ com/reader/XmlFileProperty.java
@@ -0,0 +1,25 @@
+package com.reader;
+
+public class XmlFileProperty {
+
+	public static final String READER_PARAM = "ReaderParam";
+
+	public static final String CONNECT_TIME_OUT = "ConnectTimeOut";
+
+	public static final String SLEEP_TIME_OUT = "SleepTimeOut";
+	public static final String RETRY_TIME_OUT = "RetryTimeOut";
+	public static final String TC_SEPARATE_SIGN = "TcSeparateSign";
+	public static final String REVERSE_GEOCODING_FLAG = "ReverseGeocodingFlag";
+	public static final String STATE = "State";
+	public static final String CITY = "City";
+	public static final String CITY_SET = "CitySet";
+	public static final String DATA_URL_LIST = "DataURLList";
+	public static final String DATA_URL = "DataURL";
+	public static final String MAP_URL = "MapURL";
+	public static final String PROCESSOR = "Processor";
+	public static final String REQUESTER = "Requester";
+	public static final String EXTRACTER = "Extracter";
+	public static final String FORMATTER = "Formatter";
+	public static final String REFINER = "Refiner";
+
+}
